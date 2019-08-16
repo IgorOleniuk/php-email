91ccc74dcf2c907e22877217e296acba3eeda2b0
Igor Oleniuk <igor.oleniuk1@gmail.com>
2019-08-17 00:03:15 +0300

    fourth commit

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
-$to = 'igor@igoroleniul.com.ua';
+$to = 'ivanigorolenyk@gmail.com';
 $subject = 'Заголовок письма';
 $message = 'Текст письма. Самое простое сообщение.';
-$from = 'ivanigorolenyk@gmail.com';
-$headers = 'From: $from\r\nReplay-to: %from\r\nContent-type: text-plain; charset= utf-8\r\n';
+
+$headers = 'Content-type: text-plain; charset= utf-8\r\n';
 
 mail($to, $subject, $message, $headers);
 ?>
